f2eb6c0de06273b052b4e614da2354ccc04328a2
stiansrepo <noises@gmail.com>
2015-04-30 20:27:58 +0200

    aegaeaeh

--- src/dgr/Coordinate.java
+++ src/dgr/Coordinate.java
@@ -1,22 +1,30 @@
 package dgr;
 
  // @author laptopng34
- 
 public class Coordinate {
 
-    private int x;
-    private int y;
-    
-    public Coordinate(int x,int y){
-        this.x=x;
-        this.y=y;
+    int x;
+    int y;
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
-    
-    public int getX(){
+
+    public int getX() {
         return x;
     }
-    
-    public int getY(){
+
+    public int getY() {
         return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Coordinate) {
+            Coordinate c = (Coordinate) o;
+            return c.x == x && c.y == y;
+        }
+        return false;
     }
 }
